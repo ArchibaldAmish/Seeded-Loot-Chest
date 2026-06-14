86b895b5aab93304ef76dfea7a27d294b06e2cb7
GGblameLag <garygandelev@gmail.com>
2026-06-13 22:35:02 -0700

    Register loot chest command on mod initialization

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,7 @@
 import "./commands/lootchest.js";
+import { registerLootChestCommand } from "./commands/lootchestCommand.js";
+
+// Call this when your mod initializes
+registerLootChestCommand();
 
 console.warn("[LootChest] Loaded successfully.");
